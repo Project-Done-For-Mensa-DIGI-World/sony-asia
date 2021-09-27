4d29bd5b83307d473cac5651d5cea2ec85785463
iftakharahmedchowdhury <iftakharahmedchowdhuryimacc@gmail.com>
2021-09-27 17:34:49 +0600

    first commit

---
+++ js/customJs.js
@@ -0,0 +1,22 @@
+const printerPart=()=>{
+    // console.log("I am printer")
+    const mainDiv= document.getElementById('printersOrPc');
+    document.getElementById("printerProduct").style.display = "block"; 
+    document.getElementById("pcProduct").style.display = "none"; 
+    
+ 
+}
+printerPart();
+
+const PCSPart=()=>{
+    // console.log("I am printer")
+    const mainDiv= document.getElementById('printersOrPc');
+    document.getElementById("printerProduct").style.display = "none"; 
+    document.getElementById("pcProduct").style.display = "block"; 
+   
+  
+}
+
+
+////search
+
